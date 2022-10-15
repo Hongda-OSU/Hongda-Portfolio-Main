727a6edf53ef3eb066fa275a3198d756290de716
Hongda-OSU <linhongda77@gmail.com>
2022-10-15 01:15:27 -0400

    test

--- src/components/Resume/References.js
+++ src/components/Resume/References.js
@@ -1,12 +1,27 @@
 import React from 'react';
 
+const onButtonClick = () => {
+  fetch('Resume.pdf').then((response) => {
+    response.blob().then((blob) => {
+      const fileURL = window.URL.createObjectURL(blob);
+      const alink = document.createElement('a');
+      alink.href = fileURL;
+      alink.download = 'Resume.pdf';
+      alink.click();
+    });
+  });
+};
+
 const References = () => (
   <div className="references">
     <div className="link-to" id="references" />
     <div className="title">
       <h3>
         Download My
-        <a download href="doc/Resume.pdf"> Resume </a> | <a download href="doc/Transcript.pdf"> Transcript </a>
+        <a href="https://drive.google.com/drive/u/0/folders/1VCmLyxUU3pHi257SZciMLcln0XffP5qR"> Resume | Transcript </a>
+      </h3>
+      <h3>
+        <button type="button" onClick={onButtonClick}>Download PDF</button>
       </h3>
     </div>
   </div>
