eccff2fc95ea2a4c3598abf1b3242c9e9649e2b4
Hongda-OSU <linhongda77@gmail.com>
2022-10-17 00:57:06 -0400

    work on projects

--- src/components/Projects/Cell.js
+++ src/components/Projects/Cell.js
@@ -1,20 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import dayjs from 'dayjs';
+import ReactPlayer from 'react-player/lazy';
+
+const videoControl = true;
+
+const handleImage = (data) => (
+  <>
+    <a href={data.link} className="image">
+      <img src={`${process.env.PUBLIC_URL}${data.image}`} alt={data.title} />
+    </a>
+    <div className="description">
+      <p>{data.desc}</p>
+    </div>
+  </>
+);
+
+const handleVideo = (data) => (
+  <>
+    <a href={data.link} className="image" target="_blank" rel="noopener noreferrer">
+      <ReactPlayer url={data.video} controls={videoControl} width="100%" height="480px" />
+    </a>
+    <div className="video">
+      <p>{data.desc}</p>
+    </div>
+  </>
+);
 
 const Cell = ({ data }) => (
   <div className="cell-container">
     <article className="mini-post">
       <header>
         <h3><a href={data.link}>{data.title}</a></h3>
-        <time className="published">{dayjs(data.date).format('MMMM, YYYY')}</time>
+        <time className="published">{dayjs(data.dateBegin).format('MMMM, YYYY')} - {dayjs(data.dateEnd).format('MMMM, YYYY')}</time>
       </header>
-      <a href={data.link} className="image">
-        <img src={`${process.env.PUBLIC_URL}${data.image}`} alt={data.title} />
-      </a>
-      <div className="description">
-        <p>{data.desc}</p>
-      </div>
+      { data.image.length === 0 ? handleVideo(data) : handleImage(data)}
     </article>
   </div>
 );
@@ -23,8 +43,10 @@ Cell.propTypes = {
   data: PropTypes.shape({
     title: PropTypes.string.isRequired,
     link: PropTypes.string,
+    video: PropTypes.string,
     image: PropTypes.string.isRequired,
-    date: PropTypes.string.isRequired,
+    dateBegin: PropTypes.string.isRequired,
+    dateEnd: PropTypes.string.isRequired,
     desc: PropTypes.string.isRequired,
   }).isRequired,
 };
