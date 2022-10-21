9075523f8ac0862a1e12e7b30fc4d9b25bd91c88
Hongda-OSU <linhongda77@gmail.com>
2022-10-21 06:23:50 -0400

    work on projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -6,6 +6,75 @@ const data = [
     image: '',
     link: 'https://polygamers.itch.io/karma',
     video: 'https://www.youtube.com/watch?v=uDoa5-KkLkE',
+    dateBegin: '2022-1-05',
+    dateEnd: '2022-5-13',
+    desc:
+      'Built for a social impact hackathon. '
+      + 'NearestDollar connected to your bank accounts, credit cards, '
+      + 'or debit cards and rounded up your purchases to donate the balance to '
+      + 'the charity of your choice.',
+  },
+  {
+    title: 'Medical Image Helper',
+    subtitle: 'CSE 5546 - Virtual Reality',
+    image: '',
+    link: 'https://github.com/Hongda-OSU/CSE-5546-AR-Eye-Tracking',
+    video: 'https://www.youtube.com/watch?v=wyJaXIFSe5I&t=88s',
+    dateBegin: '2022-5-05',
+    dateEnd: '2022-4-15',
+    desc:
+      'Built for a social impact hackathon. '
+      + 'NearestDollar connected to your bank accounts, credit cards, '
+      + 'or debit cards and rounded up your purchases to donate the balance to '
+      + 'the charity of your choice.',
+  },
+  {
+    title: 'Endless Runner',
+    subtitle: 'CSE 3541 - Computer Game and Animation',
+    image: '',
+    link: 'https://github.com/Hongda-OSU/CSE3541-Endless-Runner',
+    video: 'https://www.youtube.com/watch?v=J23FXJWjgFQ&t=36s',
+    dateBegin: '2021-11-20',
+    dateEnd: '2022-12-05',
+    desc:
+      'Built for a social impact hackathon. '
+      + 'NearestDollar connected to your bank accounts, credit cards, '
+      + 'or debit cards and rounded up your purchases to donate the balance to '
+      + 'the charity of your choice.',
+  },
+  {
+    title: 'AI Agents and Steering',
+    subtitle: 'CSE 3541 - Computer Game and Animation',
+    image: '',
+    link: 'https://github.com/Hongda-OSU/CSE3541-Lab5-AI-Agents',
+    video: 'https://www.youtube.com/watch?v=9Ru5QHfA88Q',
+    dateBegin: '2021-10-15',
+    dateEnd: '2022-10-25',
+    desc:
+      'Built for a social impact hackathon. '
+      + 'NearestDollar connected to your bank accounts, credit cards, '
+      + 'or debit cards and rounded up your purchases to donate the balance to '
+      + 'the charity of your choice.',
+  },
+  {
+    title: 'The Legend of Zelda (Clone)',
+    subtitle: 'CSE 3902 Project - Interactive Systems',
+    image: '',
+    link: 'https://github.com/Hongda-OSU/CSE-3902-Team-JellyLake-Zelda-',
+    video: 'https://www.youtube.com/watch?v=qvB6drBRjdc',
+    dateBegin: '2021-09-03',
+    dateEnd: '2022-12-13',
+    desc:
+      'Built for a social impact hackathon. '
+      + 'NearestDollar connected to your bank accounts, credit cards, '
+      + 'or debit cards and rounded up your purchases to donate the balance to '
+      + 'the charity of your choice.',
+  },
+  {
+    title: 'CraftMine (Mincraft Clone)',
+    subtitle: 'Personal Project',
+    link: 'https://github.com/Hongda-OSU/CraftMine',
+    image: '/images/projects/CraftMine.gif',
     dateBegin: '2022-11-20',
     dateEnd: '2022-11-20',
     desc:
@@ -15,11 +84,38 @@ const data = [
       + 'the charity of your choice.',
   },
   {
-    title: 'Nearest Dollar',
-    subtitle: '2015 BVP Hackathon',
-    image: '/images/projects/nearestdollar.jpg',
-    dateBegin: '2022-11-20',
-    dateEnd: '2022-11-20',
+    title: 'L-System Generator',
+    subtitle: '',
+    link: 'https://github.com/Hongda-OSU/CSE5542-L-System',
+    image: '/images/projects/LSystem.gif',
+    dateBegin: '2022-09-03',
+    dateEnd: '2022-10-21',
+    desc:
+      'Built for a social impact hackathon. '
+      + 'NearestDollar connected to your bank accounts, credit cards, '
+      + 'or debit cards and rounded up your purchases to donate the balance to '
+      + 'the charity of your choice.',
+  },
+  {
+    title: 'Buckeye Schedule',
+    subtitle: '',
+    link: 'https://github.com/Hongda-OSU/CSE5236-Buckeye-Schedule',
+    image: '/images/projects/BuckeyeSchedule.gif',
+    dateBegin: '2022-09-03',
+    dateEnd: '2022-10-21',
+    desc:
+      'Built for a social impact hackathon. '
+      + 'NearestDollar connected to your bank accounts, credit cards, '
+      + 'or debit cards and rounded up your purchases to donate the balance to '
+      + 'the charity of your choice.',
+  },
+  {
+    title: 'Energy & Stock Data Visualization',
+    subtitle: '',
+    link: 'https://github.com/Hongda-OSU/CSE5544-Data-Visualization',
+    image: '/images/projects/D3.png',
+    dateBegin: '2022-09-03',
+    dateEnd: '2022-10-21',
     desc:
       'Built for a social impact hackathon. '
       + 'NearestDollar connected to your bank accounts, credit cards, '
